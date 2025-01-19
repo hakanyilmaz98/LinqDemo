686fee1bd5d2edbcad871392cfce9076374bb0e5
Hakan Yilmaz <hyilmaz1998@gmail.com>
2025-01-19 18:17:07 +0100

    add initial data

---
+++ LinqDemo/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace LinqDemo;
+public class Employee
+{
+    public required string Name { get; set; }
+    public required Department Department { get; set; }
+    public required double Salary { get; set; }
+}
+
+public enum Department
+{
+    HR,
+    IT,
+    Sales,
+    Finance,
+    Marketing
+}
--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LinqDemo;
+
+List<Employee> employees = [
+    new() { Name = "John", Department = Department.HR, Salary = 50000 },
+    new() { Name = "Jane", Department = Department.IT, Salary = 75000 },
+    new() { Name = "Emily", Department = Department.Finance, Salary = 65000 },
+    new() { Name = "Michael", Department = Department.IT, Salary = 80000 },
+    new() { Name = "Chris", Department = Department.HR, Salary = 45000 },
+    new() { Name = "Sarah", Department = Department.Marketing, Salary = 55000 },
+    new() { Name = "James", Department = Department.Sales, Salary = 70000 },
+    new() { Name = "Patricia", Department = Department.Finance, Salary = 67000 },
+    new() { Name = "Robert", Department = Department.IT, Salary = 82000 },
+    new() { Name = "Linda", Department = Department.Marketing, Salary = 60000 },
+    ];
