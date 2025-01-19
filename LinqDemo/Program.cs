44b52cd095862dfe18d51309271769d338e7b9db
Hakan Yilmaz <hyilmaz1998@gmail.com>
2025-01-19 20:12:22 +0100

    distinc

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -1,24 +1,44 @@
-﻿using Dumpify;
-using LinqDemo;
+﻿using LinqDemo;
 
 List<Employee> employees = [
     new() { Name = "John", Department = Department.HR, Salary = 50000 },
-    new() { Name = "Jane", Department = Department.IT, Salary = 75000 },
-    new() { Name = "Emily", Department = Department.Finance, Salary = 65000 },
-    new() { Name = "Michael", Department = Department.IT, Salary = 80000 },
-    new() { Name = "Chris", Department = Department.HR, Salary = 45000 },
-    new() { Name = "Sarah", Department = Department.Marketing, Salary = 55000 },
-    new() { Name = "James", Department = Department.Sales, Salary = 70000 },
-    new() { Name = "Patricia", Department = Department.Finance, Salary = 67000 },
-    new() { Name = "Robert", Department = Department.IT, Salary = 82000 },
-    new() { Name = "Linda", Department = Department.Marketing, Salary = 60000 },
+    new() { Name = "John", Department = Department.IT, Salary = 75000 },
+    new() { Name = "John", Department = Department.Finance, Salary = 65000 },
+    new() { Name = "John", Department = Department.IT, Salary = 80000 },
+    new() { Name = "Sara", Department = Department.HR, Salary = 45000 },
+    new() { Name = "Sara", Department = Department.Marketing, Salary = 55000 },
+    new() { Name = "Sara", Department = Department.Sales, Salary = 70000 },
+    new() { Name = "Sara", Department = Department.Finance, Salary = 67000 },
+    new() { Name = "Sara", Department = Department.IT, Salary = 82000 },
+    new() { Name = "Sara", Department = Department.Marketing, Salary = 60000 },
     ];
 
-var result = employees.Sum(x => x.Salary);
+//var result = employees.Select(x => x.Name).Distinct();
+//result.Dump();
+
+//var result = employees.OrderByDescending(x => x.Salary).Skip(7).Take(3);
+//var result = employees.OrderByDescending(x => x.Salary).Take(3);
+
+//var result = employees.Index();
+
+//var result = employees.CountBy(x => x.Department);
+//var result = employees.MaxBy(x => x.Department);
+//var result = employees.MinBy(x => x.Department);
+
+//var result = employees.OrderByDescending(x => x.Salary).Skip(9).Take(1);
+//var result = employees.OrderByDescending(x => x.Salary).Take(3);
+
+//var result = employees.Count(x => x.Salary >= 75_000);
+//var result = employees.First(x => x.Name.StartsWith('P'));
+//var result = employees.OrderByDescending(x => x.Salary).First();
+//var result = employees.OrderByDescending(x => x.Salary).First();
+//var result = employees.OrderBy(x => x.Salary).Last();
+
+
+//var result = employees.Sum(x => x.Salary);
 //var result = employees.Min(x => x.Salary);
 //var result = employees.Max(x => x.Salary);
 //var result = employees.Average(x => x.Salary);
-result.Dump();
 
 //var result = employees.All(x => x.Salary >= 30_000);
 //var result = employees.Any(x => x.Salary < 20_000);
